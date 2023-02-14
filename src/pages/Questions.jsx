1b13cf7f4b337465ad4bb3e63396600b701992fd
Gobinath7878 <gobinaths7878@gmail.com>
2023-02-14 16:11:57 +0530

    end commit

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -33,7 +33,7 @@ const Questions = () => {
   useEffect(() => {
     const fetchQuestions = async () => {
       try {
-        const response = await axios.get(`${BASE_URL}/api/v1/questions`);
+        const response = await axios.get(`${BASE_URL}/questions`);
         setQuestions(response.data.data);
       } catch (error) {
         // console.error(error);
